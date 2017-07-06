7db4d0bbd1233c886d19330f14b6228b2c50e706
root <root@bastion.mon.internal>
2017-07-06 18:36:24 -0400

    Added VOLUME_CAPACITY

--- grafana.sh
+++ grafana.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 oc new-project grafana --display-name="Monitoring - Grafana Dashboards"
 oc create -f grafana.yaml
-oc new-app grafana -p NAMESPACE=grafana
+oc new-app grafana -p NAMESPACE=grafana -p VOLUME_CAPACITY=4Gi
