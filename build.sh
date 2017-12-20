0d4c81a795ad4b94b5f5c49aa8584733c5e4d6e9
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-12-20 09:29:16 -0500

    Updated to Grafana 4.6.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t wkulhanek/grafana:latest
-docker tag wkulhanek/grafana:latest wkulhanek/grafana:4.6.2
+docker tag wkulhanek/grafana:latest wkulhanek/grafana:4.6.3
 docker push wkulhanek/grafana:latest
-docker push wkulhanek/grafana:4.6.2
+docker push wkulhanek/grafana:4.6.3
