ea97b626281688a8849da59f8e2289b501ea66d4
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-06-30 09:56:49 -0400

    Updated to Grafana 5.2.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=5.2.0
+export VERSION=5.2.1
 docker build . -t wkulhanek/grafana:latest
 docker tag wkulhanek/grafana:latest wkulhanek/grafana:${VERSION}
 docker push wkulhanek/grafana:${VERSION}
